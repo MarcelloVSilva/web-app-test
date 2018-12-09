2271000b76a70c0004b9da7e319c0af169ac49a6
MarcelloVSilva <marcellovcs@gmail.com>
2018-12-09 04:06:33 -0200

    validacao de campos no contato

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
-import { TextField } from '@material-ui/core'
+import { TextField, Button } from '@material-ui/core'
 import { withLocalize, Translate } from 'react-localize-redux';
 import ContactTranslations from '../../res/translations/contact.json'
 import { setAbaAtiva } from '../../res/funcs/setAbaAtiva.js';
+
+import './Contact.css'
 
 class Contact extends Component {
     constructor(props) {
         super(props)
         props.addTranslation(ContactTranslations)
         this.state = {
-            contactForm: {}
+            contactForm: {},
+            camposObrigatorios: [
+                {
+                    campo: 'nome',
+                    temErro: false
+                },
+                {
+                    campo: 'email',
+                    temErro: false
+                }]
+                
         }
+        this.salvar = this.salvar.bind(this)
+        this.validaCampos = this.validaCampos.bind(this)
     }
 
     componentDidMount = () => {
@@ -25,33 +39,93 @@ class Contact extends Component {
         });
     }
 
+    async salvar() {
+        try {
+            await this.validaCampos()
+            const camposObrigatorios = [
+                {
+                    campo: 'nome',
+                    temErro: false
+                },
+                {
+                    campo: 'email',
+                    temErro: false
+                }]
+            this.setState({ camposObrigatorios })
+            const card = document.getElementsByClassName('card')[0]
+            card.classList.toggle('rotated');
+        } catch {
+        }
+    }
+
+    validaCampos() {
+        return new Promise((resolve) => {
+            const { camposObrigatorios, contactForm } = this.state
+            let contErros = 2
+            let errosCampos = []
+            let erro = {
+                campo: '',
+                temErro: ''
+            }
+            camposObrigatorios.some((campoObrigatorio) => {
+                const valorDoCampo = contactForm[campoObrigatorio.campo]
+                if (campoObrigatorio.campo === 'nome' && (!valorDoCampo || !(/[A-Za-z]/).test(valorDoCampo) || valorDoCampo === '')) {
+                    campoObrigatorio.temErro = true
+                    errosCampos.push(erro)
+                } else if (campoObrigatorio.campo === 'email' && (!valorDoCampo || (/[@]^/).test(valorDoCampo) || valorDoCampo === '')) {
+                    campoObrigatorio.temErro = true
+                    errosCampos.push(erro)
+                } else contErros--
+            })
+            contErros === 0 ? resolve() : this.setState({ camposObrigatorios })
+        })
+    }
+
     render() {
-        const { contactForm } = this.state
+        const { contactForm, camposObrigatorios } = this.state
         return (
             <Translate>
                 {({ translate }) => {
                     return (
-                        <div id='contactForm'>
-                            <TextField
-                                autoFocus={true}
-                                onChange={this.handleChange('nome')}
-                                value={contactForm.nome || ''}
-                                id='nome'
-                                label={`${translate('nome')} *`} />
-                            <TextField
-                                onChange={this.handleChange('email')}
-                                value={contactForm.email || ''}
-                                id='email'
-                                label={`${translate('email')} *`} />
-                            <TextField
-                                multiline
-                                rows={4}
-                                onChange={this.handleChange('descricao')}
-                                value={contactForm.descricao || ''}
-                                id='descricao'
-                                label={translate('descricao')} />
-                        </div>
+                        <div id='contactPage'>
+                            <div className="card">
+                                <div className="card-contents card-front" >
+                                    <div className="card-depth">
+                                        <div id='contactForm'>
+                                            <TextField
+                                                error={camposObrigatorios[0].temErro && true}
+                                                helperText={camposObrigatorios[0].temErro && translate('erroNome')}
+                                                autoFocus={true}
+                                                onChange={this.handleChange('nome')}
+                                                value={contactForm.nome || ''}
+                                                id='nome'
+                                                label={`${translate('nome')} *`} />
+                                            <TextField
+                                                error={camposObrigatorios[1].temErro && true}
+                                                helperText={camposObrigatorios[1].temErro && translate('erroEmail')}
+                                                onChange={this.handleChange('email')}
+                                                value={contactForm.email || ''}
+                                                id='email'
+                                                label={`${translate('email')} *`} />
+                                            <TextField
+                                                multiline
+                                                rows={4}
+                                                onChange={this.handleChange('descricao')}
+                                                value={contactForm.descricao || ''}
+                                                id='descricao'
+                                                label={translate('descricao')} />
+                                        </div>
+                                        <Button onClick={this.salvar}>{translate('enviar')}</Button>
+                                    </div>
+                                </div>
 
+                                <div className="card-contents card-back">
+                                    <div className="card-depth">
+                                        Sucesso
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     )
                 }}
             </Translate>
