250a11d6625babf7496f7f06eefe2a9fbfad7786
MarcelloVSilva <marcellovcs@gmail.com>
2018-12-09 01:05:55 -0200

    melhorias do header, logo react e modificacao de alguns css's

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
-import { Switch, Route, Link} from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Routes from './Routes';
 import { Translate, withLocalize } from 'react-localize-redux';
 import GlobalTranslations from './res/translations/global.json'
 import bdbr from './res/bandeiras/bd-br.png'
 import bdusa from './res/bandeiras/bd-usa.png'
+import LogoReact from './res/logo/logoReact.svg'
 
 class App extends Component {
   constructor(props) {
@@ -28,23 +29,25 @@ class App extends Component {
           return (
             <div className="App">
               <div className='App-header'>
-                <i id='globo' className="fas fa-globe-americas"></i>
-                <div id='languagePick'>
-                  <div onClick={() => translate.setActiveLanguage('pt')}>
-                    <img alt='flag' className="bandeiras bandeiraBrasil" src={bdbr} />
-                  </div>
-                  <div onClick={() => translate.setActiveLanguage('en')}>
-                    <img alt='flag' className="bandeiras bandeiraUSA" src={bdusa} />
-                  </div>
-                </div>
-                <div id='logo'>LOGO</div>
                 <div id='navigation'>
+                  <img src={LogoReact} className='App-logo' ></img>
                   <Link to='/'>
                     <div>{translate.translate('home')}</div>
                   </Link>
                   <Link to='/contact'>
                     <div>{translate.translate('contact')}</div>
                   </Link>
+                </div>
+                <div id='changeLanguage'>
+                  <i id='globo' className="fas fa-globe-americas"></i>
+                  <div id='languagePick'>
+                    <div onClick={() => translate.setActiveLanguage('pt')}>
+                      <img alt='flag' className="bandeiras bandeiraBrasil" src={bdbr} />
+                    </div>
+                    <div onClick={() => translate.setActiveLanguage('en')}>
+                      <img alt='flag' className="bandeiras bandeiraUSA" src={bdusa} />
+                    </div>
+                  </div>
                 </div>
               </div>
               <main>
